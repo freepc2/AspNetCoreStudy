234c85fa02e4c93b02edbef8cc4c32790ab85468
freepc2 <freepc2@hotmail.com>
2020-12-28 00:17:46 +0900

    first commit

---
+++ AspNetCoreStudy/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using AspNetCoreStudy.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreStudy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        /// <summary>
+        ///  http://www.example.com/Home/Index
+        /// </summary>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        ///  http://www.example.com/Home/Privacy
+        /// </summary>
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        /// <summary>
+        ///  http://www.example.com/Home/Error
+        /// </summary>
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AspNetCoreStudy/Controllers/StudyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreStudy.Controllers
+{
+    public class StudyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Java()
+        {
+            return View();
+        }
+
+        public IActionResult CSharp()
+        {
+            return View();
+        }
+
+        public IActionResult Cpp()
+        {
+            return View();
+        }
+    }
+}
